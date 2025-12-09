0140d40f50b316803930d2005c583faee8f839c2
John Nkanu <41162489+jondon09@users.noreply.github.com>
2025-12-09 12:09:24 +0000

    updated the aws provider

--- provider.tf
+++ provider.tf
@@ -10,7 +10,7 @@ terraform {
 terraform {
   backend "s3" {
     bucket         = "terraform-backend-31112025"
-    key            = "src-terraform-aws-project/terraform.tfstate"
+    key            = "src-terraform-aws-ssm-project/terraform.tfstate"
     region         = "us-east-1"
     profile        = "iamadmin-general"
     dynamodb_table = "terraform_state_lock"
