c9e22a2697bc6fb290111bbecc0e810954418fe3
John Nkanu <41162489+jondon09@users.noreply.github.com>
2025-12-17 09:31:40 +0000

    updated networking module

---
+++ main.tf
@@ -0,0 +1,11 @@
+# Create VPC Network
+module "networking" {
+  source = "./modules/vpc"
+
+  vpc_config = {
+    cidr_block           = "10.0.0.0/16"
+    enable_dns_hostnames = true
+    enable_dns_support   = true
+    availability_zones   = ["us-east-1a", "us-east-1b"]
+  }
+}
