136a1e13d04ae6510efca2cc60c319c275a68c23
John Nkanu <41162489+jondon09@users.noreply.github.com>
2025-12-17 11:56:22 +0000

    updated networking module

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,7 +1,7 @@
 # Local variable to store the number of public and private subnets
 locals {
-  num_of_public_subnet  = length(var.vpc_config.availability_zones)
-  num_of_private_subnet = length(var.vpc_config.availability_zones)
+  num_of_public_subnets  = length(var.vpc_config.availability_zones)
+  num_of_private_subnets = length(var.vpc_config.availability_zones)
 }
 
 # data source for current aws region
@@ -22,3 +22,17 @@ resource "aws_internet_gateway" "igw" {
     "Name" = "cf_igw"
   }
 }
+
+### Public Subnet ###
+
+resource "aws_subnet" "public" {
+  count             = local.num_of_public_subnets
+  vpc_id            = aws_vpc.vpc.id
+  availability_zone = var.vpc_config.availability_zones[count.index]
+  cidr_block        = cidrsubnet(aws_vpc.vpc.cidr_block, 2, count.index + 1) # automatically creates VPC CIDR based on CIDR Block
+  tags = {
+    "Name" = "cf_public_subnet_${count.index + 1}"
+  }
+
+}
+
