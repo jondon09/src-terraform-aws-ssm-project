737b39fe595337392c7640812e52c751fa9419b1
John Nkanu <41162489+jondon09@users.noreply.github.com>
2025-12-17 11:58:12 +0000

    updated networking module

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -36,3 +36,11 @@ resource "aws_subnet" "public" {
 
 }
 
+# Route table for Public subnet
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw
+  }
+}
